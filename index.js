dc4683017e692b859ca0e00a8b5129dc239ee30a
GamagamiDev <81793604+cgamagami1@users.noreply.github.com>
2023-05-01 23:14:32 -0500

    Add commenting functionality

--- index.js
+++ index.js
@@ -15,8 +15,33 @@ const con = await mysql.createConnection({
     password: "MyServer123"
 });
 
+const formatResults = (results) => {
+    const posts = [];
+    let currentPost;
+
+    for (const result of results) {
+        const { postID, postContent, photoURL, postDate, location, commentID, commentContent, commentDate } = result;
+        if (postID !== currentPost) {
+            currentPost = result.postID;
+            posts.push({ postID, postContent, photoURL, postDate, location, comments: [] });
+        }
+
+        if (commentID !== null) {
+            posts[posts.length - 1].comments.push({ commentID, commentContent, commentDate });
+        }
+    }
+
+    return posts;
+}
+
 app.get("/posts", async (req, res) => {
-    const [posts] = await con.query("SELECT * FROM post ORDER BY date ASC");
+    const [results] = await con.query(
+        "SELECT post.postID, post.content AS postContent, post.photoURL, post.date AS postDate, post.location, " +
+        "comment.commentID, comment.content AS commentContent, comment.date AS commentDate " +
+        "FROM post LEFT OUTER JOIN comment ON post.postID = comment.postID"
+    );
+
+    const posts = formatResults(results);
 
     res.render("posts", { posts });
 })
@@ -26,17 +51,24 @@ app.get("/search", async (req, res) => {
         res.render("search", { posts: [] });
         return;
     }
+
+    const [results] = await con.query(
+        "SELECT post.postID, post.content AS postContent, post.photoURL, post.date AS postDate, post.location, " +
+        "comment.commentID, comment.content AS commentContent, comment.date AS commentDate " +
+        "FROM post LEFT OUTER JOIN comment ON post.postID = comment.postID " +
+        "WHERE post.content LIKE ?", ['%' + req.query.content + '%']
+    );
     
-    const [posts] = await con.query("SELECT * FROM post WHERE content LIKE ? ORDER BY date ASC", ['%' + req.query.content + '%']);
+    const posts = formatResults(results);
 
     res.render("search", { posts });
 })
 
-app.get("/new", (req, res) => {
-    res.render("new");
+app.get("/newpost", (req, res) => {
+    res.render("newpost");
 })
 
-app.post("/new", (req, res) => {
+app.post("/newpost", (req, res) => {
     const values = [
         req.body.content, 
         req.body.photoURL === "" ? null : req.body.photoURL, 
@@ -45,18 +77,31 @@ app.post("/new", (req, res) => {
     ]
 
     con.query("INSERT INTO post (content, photoURL, date, location) VALUES (?)", [values]);
-    res.render("new");
+    res.render("newpost");
 })
 
-app.post("/delete", async (req, res) => {
-    await con.query("DELETE FROM post WHERE postID=?", [req.body.postID]);
-    res.send("success");
+app.post("/newcomment/:postID", async (req, res) => {
+    const values = [req.params.postID, req.body.content, new Date().toISOString().substring(0, 10)];
+
+    await con.query("INSERT INTO comment (postID, content, date) VALUES (?)", [values]);
+    res.redirect(req.headers.referer);
 })
 
-app.post("/update", async (req, res) => {
-    await con.query("UPDATE post SET ? WHERE postID=?", [{ content: req.body.content, photoURL: req.body.photoURL, location: req.body.location }, req.body.postID]);
+app.post("/deletepost/:postID", async (req, res) => {
+    await con.query("DELETE FROM comment WHERE postID=?", [req.params.postID]);
+    await con.query("DELETE FROM post WHERE postID=?", [req.params.postID]);
+    res.redirect(req.headers.referer);
+})
+
+app.post("/updatepost/:postID", async (req, res) => {
+    await con.query("UPDATE post SET ? WHERE postID=?", [{ content: req.body.content, photoURL: req.body.photoURL, location: req.body.location }, req.params.postID]);
     res.redirect(req.headers.referer);
 });
+
+app.post("/deletecomment/:commentID", async (req, res) => {
+    await con.query("DELETE FROM comment WHERE commentID=?", [req.params.commentID]);
+    res.redirect(req.headers.referer);
+})
 
 app.listen(3000, () => {
     console.log('App listening on port 3000!');
--- public/script.js
+++ public/script.js
@@ -1,13 +1,3 @@
-const deletePost = async (e) => {
-    const postID = e.target.parentNode.parentNode.id;
-
-    await fetch("/delete", {
-        method: "POST",
-        body: JSON.stringify({ postID })
-    })
-
-    window.location.replace("/recent");
-}
 
 const toggleEditPost = (e) => {
     const formElement = e.target.parentNode.parentNode.nextElementSibling;
@@ -15,11 +5,17 @@ const toggleEditPost = (e) => {
     formElement.classList.toggle("hidden");
 }
 
+const toggleShowComments = (e) => {
+    const commentElement = e.target.parentNode.parentNode.nextElementSibling.nextElementSibling;
+
+    commentElement.classList.toggle("hidden");
+}
+
 (function initializeApp() {
-    for (const element of document.querySelectorAll(".delete")) {
-        element.addEventListener("click", deletePost);
-    }
     for (const element of document.querySelectorAll(".edit")) {
         element.addEventListener("click", toggleEditPost);
+    }
+    for (const element of document.querySelectorAll(".show-comments")) {
+        element.addEventListener("click", toggleShowComments);
     }
 })();
